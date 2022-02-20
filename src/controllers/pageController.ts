d20c50749bb62c6548860ab20f1f5c44d553b22a
Wilson Junior <wilsonraj@unipam.edu.br>
2022-02-20 20:31:08 -0300

    Separando as views

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,17 +1,17 @@
 import { Request, Response } from 'express';
 
 export const home = (req: Request, res: Response) => {
-
+    res.render('pages/page');
 }
 
 export const dogs = (req: Request, res: Response) => {
-
+    res.send('dogs');
 }
 
 export const cats = (req: Request, res: Response) => {
-
+    res.send('cats');
 }
 
 export const fish = (req: Request, res: Response) => {
-
+    res.send('fish');
 }
